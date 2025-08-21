1749084adf0bfd0277872cdb31b6d4dddcaad120
md-ahmodullah <contactme.ahmod@gmail.com>
2025-08-22 02:03:01 +0600

    Added: Secret

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,61 +1,3 @@
-// import bcrypt from "bcryptjs";
-// import NextAuth, { AuthOptions } from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import GoogleProvider from "next-auth/providers/google";
-// import { connectDB } from "./mongodb";
-// import User from "./User";
-
-// export const authOptions: AuthOptions = {
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//     CredentialsProvider({
-//       name: "Credentials",
-//       credentials: {
-//         email: { label: "Email", type: "email" },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials) {
-//         await connectDB();
-//         const user = await User.findOne({ email: credentials?.email });
-//         if (!user) throw new Error("Invalid credentials");
-
-//         const isValid = await bcrypt.compare(
-//           credentials!.password,
-//           user.password
-//         );
-//         if (!isValid) throw new Error("Invalid credentials");
-
-//         return { id: user._id, name: user.name, email: user.email };
-//       },
-//     }),
-//   ],
-//   pages: {
-//     signIn: "/login",
-//   },
-//   session: {
-//     strategy: "jwt",
-//   },
-//   // callbacks: {
-//   //   async jwt({ token, user }) {
-//   //     if (user) {
-//   //       token.id = user.id;
-//   //     }
-//   //     return token;
-//   //   },
-//   //   async session({ session, token }) {
-//   //     if (token) {
-//   //       session.user.id = token.id as string;
-//   //     }
-//   //     return session;
-//   //   },
-//   // },
-// };
-
-// export default NextAuth(authOptions);
-
 import bcrypt from "bcryptjs";
 import type { User as NextAuthUser } from "next-auth";
 import NextAuth, { AuthOptions } from "next-auth";
@@ -109,6 +51,8 @@ export const authOptions: AuthOptions = {
   pages: {
     signIn: "/login",
   },
+
+  secret: process.env.NEXTAUTH_SECRET,
 
   session: {
     strategy: "jwt",
