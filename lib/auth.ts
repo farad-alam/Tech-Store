7520347513baa4741822bc975aeba14fb28d0bbe
md-ahmodullah <contactme.ahmod@gmail.com>
2025-08-22 02:50:49 +0600

    Fixed server not found issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -64,7 +64,9 @@ export const authOptions: AuthOptions = {
       return token;
     },
     async session({ session, token }) {
-      if (token) session?.user?.id = token.id as string;
+      if (token && session.user) {
+        session.user.id = token.id as string;
+      }
       return session;
     },
   },
